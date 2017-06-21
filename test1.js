fc8bfa3dd397a639e57650cdf751540e507f3f72
A Yong <andraceyong@gmail.com>
2017-06-21 13:49:20 -0400

    commit

--- test1.js
+++ test1.js
@@ -1,1 +1,31 @@
 console.log("Hello Javascript/Node world!!!")
+console.log("This is the first line\nAnd this is the second")
+console.log("Aardvark" < "Zoroaster")
+console.log(1<2)
+console.log(NaN==NaN)
+console.log(null == undefined);
+
+console.log("------------------------------------");
+console.log(8 * null)
+// → 0
+console.log("5" - 1)
+// → 4
+console.log("5" + 1)
+// → 51
+console.log("five" * 2)
+// → NaN
+console.log(false == 0)
+// → true
+console.log("------------------------------------");
+console.log(null || "user")
+// → user
+console.log("Karl" || "user")
+// → Karl
+
+console.log("-------------Variables-----------------------");
+
+var ten=10;
+console.log("Calculate value : " + ten*ten);
+
+alert("good morning!")
+
